2e19e6d56e7cc5b22b28fc144759aef2f472fb63
Kale-Ko <github@kaleko.dev>
2024-07-02 03:17:27 -0400

    ECVelocityScheduler

---
+++ Core/src/main/java/io/github/evercraftmc/core/impl/velocity/server/ECVelocityScheduler.java
@@ -0,0 +1,65 @@
+package io.github.evercraftmc.core.impl.velocity.server;
+
+import com.velocitypowered.api.scheduler.ScheduledTask;
+import io.github.evercraftmc.core.api.server.ECProxyServer;
+import io.github.evercraftmc.core.api.server.ECScheduler;
+import java.util.concurrent.TimeUnit;
+import org.jetbrains.annotations.NotNull;
+
+public class ECVelocityScheduler implements ECScheduler { // TODO Check if tasks are run synchronously or asynchronously
+    public static class ECVelocityTask implements ECTask {
+        protected final @NotNull ScheduledTask handle;
+
+        public ECVelocityTask(@NotNull ScheduledTask handle) {
+            this.handle = handle;
+        }
+
+        public @NotNull ScheduledTask getHandle() {
+            return this.handle;
+        }
+
+        public void cancel() {
+            this.handle.cancel();
+        }
+    }
+
+    protected final @NotNull ECVelocityServer server;
+
+    public ECVelocityScheduler(@NotNull ECVelocityServer server) {
+        this.server = server;
+    }
+
+    public @NotNull ECProxyServer getServer() {
+        return this.server;
+    }
+
+    @Override
+    public @NotNull ECVelocityScheduler.ECVelocityTask runTask(@NotNull Runnable task) {
+        return new ECVelocityScheduler.ECVelocityTask(this.server.getHandle().getScheduler().buildTask(this.server.getPlugin().getHandle(), task).schedule());
+    }
+
+    @Override
+    public @NotNull ECVelocityScheduler.ECVelocityTask runTaskAsync(@NotNull Runnable task) {
+        return new ECVelocityScheduler.ECVelocityTask(this.server.getHandle().getScheduler().buildTask(this.server.getPlugin().getHandle(), task).schedule());
+    }
+
+    @Override
+    public @NotNull ECVelocityScheduler.ECVelocityTask runTaskLater(@NotNull Runnable task, int ticks) {
+        return new ECVelocityScheduler.ECVelocityTask(this.server.getHandle().getScheduler().buildTask(this.server.getPlugin().getHandle(), task).delay(ticks * 50L, TimeUnit.MILLISECONDS).schedule());
+    }
+
+    @Override
+    public @NotNull ECVelocityScheduler.ECVelocityTask runTaskLaterAsync(@NotNull Runnable task, int ticks) {
+        return new ECVelocityScheduler.ECVelocityTask(this.server.getHandle().getScheduler().buildTask(this.server.getPlugin().getHandle(), task).delay(ticks * 50L, TimeUnit.MILLISECONDS).schedule());
+    }
+
+    @Override
+    public @NotNull ECVelocityScheduler.ECVelocityTask runTaskRepeat(@NotNull Runnable task, int delay, int ticks) {
+        return new ECVelocityScheduler.ECVelocityTask(this.server.getHandle().getScheduler().buildTask(this.server.getPlugin().getHandle(), task).delay(delay * 50L, TimeUnit.MILLISECONDS).delay(ticks * 50L, TimeUnit.MILLISECONDS).schedule());
+    }
+
+    @Override
+    public @NotNull ECVelocityScheduler.ECVelocityTask runTaskRepeatAsync(@NotNull Runnable task, int delay, int ticks) {
+        return new ECVelocityScheduler.ECVelocityTask(this.server.getHandle().getScheduler().buildTask(this.server.getPlugin().getHandle(), task).delay(delay * 50L, TimeUnit.MILLISECONDS).delay(ticks * 50L, TimeUnit.MILLISECONDS).schedule());
+    }
+}
