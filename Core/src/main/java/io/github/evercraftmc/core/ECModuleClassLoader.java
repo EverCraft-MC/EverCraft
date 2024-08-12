c29f1574aa00954b4a8a3bfb31105a3f94da1ff8
Kale-Ko <github@kaleko.dev>
2024-08-12 17:47:12 -0400

    New module class loader

--- Core/src/main/java/io/github/evercraftmc/core/ECModuleClassLoader.java
+++ Core/src/main/java/io/github/evercraftmc/core/ECModuleClassLoader.java
@@ -1,67 +1,162 @@
 package io.github.evercraftmc.core;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
-import java.util.zip.ZipEntry;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class ECModuleClassLoader extends ClassLoader {
-    protected final @NotNull ClassLoader parent;
+    protected final @NotNull Path jarPath;
 
-    protected final @NotNull File file;
+    public ECModuleClassLoader(@NotNull Path jarPath) {
+        super(null);
 
-    public ECModuleClassLoader(@NotNull ClassLoader parent, @NotNull File file) {
-        this.parent = parent;
-
-        this.file = file;
+        this.jarPath = jarPath;
     }
 
-    public final @NotNull ClassLoader getParentClassLoader() {
-        return this.parent;
+    public @NotNull Path getJarPath() {
+        return this.jarPath;
     }
 
-    public @NotNull File getFile() {
-        return this.file;
-    }
+    protected boolean cataloged = false;
+    protected Map<String, List<Path>> resourceCatalog = new HashMap<>();
+    protected Map<String, List<Path>> extractedResourceCatalog = new HashMap<>();
 
-    @Override
-    protected @Nullable Class<?> findClass(@NotNull String name) throws ClassNotFoundException {
-        try {
-            try {
-                return this.parent.loadClass(name);
-            } catch (ClassNotFoundException e) {
-                byte[] data = this.loadClassData(name);
-
-                return this.defineClass(name, data, 0, data.length);
-            }
-        } catch (IOException e) {
-            throw new ClassNotFoundException(name, e);
+    protected synchronized void catalogAll() throws IOException {
+        if (this.cataloged) {
+            return;
         }
+        this.cataloged = true;
+
+        Path tempDir = Files.createTempDirectory("evercraft-");
+        tempDir.toFile().deleteOnExit();
+
+        this.catalog(this.jarPath, tempDir);
     }
 
-    protected byte[] loadClassData(@NotNull String name) throws ClassNotFoundException, IOException {
-        try (JarInputStream jar = new JarInputStream(new BufferedInputStream(new FileInputStream(this.file)))) {
-            ZipEntry entry;
-            while ((entry = jar.getNextEntry()) != null) {
-                if (entry.getName().equals(name.replace(".", "/") + ".class")) {
-                    ByteBuffer data = ByteBuffer.allocate((int) entry.getSize());
+    private synchronized void catalog(Path parentJar, Path tempDir) throws IOException {
+        List<Path> jars = new ArrayList<>();
 
-                    byte[] buf = new byte[2048];
-                    int read;
-                    while ((read = jar.read(buf)) != -1) {
-                        data.put(buf, 0, read);
+        try (JarInputStream jarInputStream = new JarInputStream(new BufferedInputStream(Files.newInputStream(parentJar)))) {
+            JarEntry jarEntry;
+            while ((jarEntry = jarInputStream.getNextJarEntry()) != null) {
+                if (!jarEntry.isDirectory()) {
+                    Path file = tempDir.resolve(parentJar.getFileName()).resolve(jarEntry.getName());
+                    if (!Files.exists(file.getParent())) {
+                        Files.createDirectories(file.getParent());
                     }
 
-                    return data.array();
+                    if (jarEntry.getName().endsWith(".jar")) {
+                        jars.add(file);
+                    }
+
+                    try (OutputStream fileOutputStream = Files.newOutputStream(file)) {
+                        int read;
+                        byte[] buf = new byte[4096];
+                        while ((read = jarInputStream.read(buf)) != -1) {
+                            fileOutputStream.write(buf, 0, read);
+                        }
+                    }
+
+                    if (!this.resourceCatalog.containsKey(jarEntry.getName())) {
+                        this.resourceCatalog.put(jarEntry.getName(), new ArrayList<>());
+                    }
+                    this.resourceCatalog.get(jarEntry.getName()).add(parentJar);
+
+                    if (!this.extractedResourceCatalog.containsKey(jarEntry.getName())) {
+                        this.extractedResourceCatalog.put(jarEntry.getName(), new ArrayList<>());
+                    }
+                    this.extractedResourceCatalog.get(jarEntry.getName()).add(file);
                 }
             }
         }
 
-        throw new ClassNotFoundException(name);
+        for (Path jar : jars) {
+            this.catalog(jar, tempDir);
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        try {
+            return this.getClass().getClassLoader().loadClass(name);
+        } catch (ClassNotFoundException e) {
+        }
+
+        String fileName = name.replace(".", "/") + ".class";
+
+        if (!(this.extractedResourceCatalog.containsKey(fileName) && !this.extractedResourceCatalog.get(fileName).isEmpty())) {
+            throw new ClassNotFoundException(name);
+        }
+
+        try (InputStream jarInputStream = Files.newInputStream(this.extractedResourceCatalog.get(fileName).get(0))) {
+            try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+                int read;
+                byte[] buf = new byte[4096];
+                while ((read = jarInputStream.read(buf)) != -1) {
+                    outputStream.write(buf, 0, read);
+                }
+
+                return defineClass(name, outputStream.toByteArray(), 0, outputStream.size());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected URL findResource(String name) {
+        {
+            URL resource = this.getClass().getClassLoader().getResource(name);
+            if (resource != null) {
+                return resource;
+            }
+        }
+
+        if (!(this.resourceCatalog.containsKey(name) && !this.resourceCatalog.get(name).isEmpty())) {
+            return null;
+        }
+
+        try {
+            Path path = this.resourceCatalog.get(name).get(0);
+
+            return new URL("jar", "", -1, "file:" + path.toAbsolutePath() + "!/" + name);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected Enumeration<URL> findResources(String name) {
+        try {
+            Enumeration<URL> resources = this.getClass().getClassLoader().getResources(name);
+            if (resources != null && resources.hasMoreElements()) {
+                return resources;
+            }
+        } catch (IOException e) {
+        }
+
+        if (!(this.resourceCatalog.containsKey(name) && !this.resourceCatalog.get(name).isEmpty())) {
+            return null;
+        }
+
+        try {
+            List<URL> resources = new ArrayList<>();
+
+            for (Path path : this.resourceCatalog.get(name)) {
+                resources.add(new URL("jar", "", -1, "file:" + path.toAbsolutePath() + "!/" + name));
+            }
+
+            return Collections.enumeration(resources);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
